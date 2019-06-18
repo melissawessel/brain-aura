642d3bb796c11deefeada9747fb78616d5fe8c0c
Melissa Wessel <melissawessel91@gmail.com>
2019-06-18 11:20:03 -0400

    clean up, split out script

---
+++ aura-generator.js
@@ -0,0 +1,69 @@
+
+var colors = [ "Red", "Orange", "Yellow", "Green",
+    "Blue", "Violet" ];
+
+// Configuration
+var margin = { top: 30, right: 30, bottom: 50, left: 30, pad: 10 }
+var width = 900 - margin.left - margin.right;
+var height = 900 - margin.top - margin.bottom;
+
+// SVG element for rainbow
+var svgContainer = d3.select(".rainbow").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .attr("transform", "translate(" + width/2 + "," + height/2 + ")");
+
+// Defining array of 6 randomized brain state scores
+var scores = Array.from({length: 6}, () => Math.floor(Math.random() * 100));
+console.log(scores);
+
+//Define arcs
+var arc = d3.arc()
+    .innerRadius(function(d, i) {
+        return 450 - 50 * i + scores[i]/2.5
+    })
+    .outerRadius(function(d, i) {
+        return 350 - 50 * i - scores[i]/2.5
+    })
+    .startAngle(function(d, i) {
+        return -Math.PI / 1.2 + .08 * i
+
+    })
+    .endAngle(function(d, i) {
+        return Math.PI / 1.2 - .08 * i
+    })
+
+// Container for blur
+var defs = svgContainer.append("defs");
+
+// Blur filter
+var filter = defs.append("filter")
+    .attr("id","glow");
+filter.append("feGaussianBlur")
+    .attr("stdDeviation","20");
+
+// Placing base image
+svgContainer.append('svg:image')
+    .attr("xlink:href", "aura-base.png")
+    .attr("x", "-450")
+    .attr("y", "-450")
+    .attr("width", "900")
+    .attr("height", "900");
+
+//Adding all colors arc
+var path = svgContainer.selectAll("g.arc")
+    .data(colors)
+    .enter().append("g")
+    .attr("class", "arc")
+    .style("opacity", function(d, i) {
+      return scores[i]/200.0;
+    })
+    .style("filter", "url(#glow)");
+
+//Adding to path
+path.append("path")
+    .attr("fill", function(d, i) {
+        return d;
+    })
+    .attr("d", arc);
